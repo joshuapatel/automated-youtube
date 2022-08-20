33e08e296871c596671a83aee733ee35df503fe0
joshuapatel <23743695+joshuapatel@users.noreply.github.com>
2022-08-20 02:28:52 -0400

    Improve validation for video IDs and rewrite Dockerfile

--- callback/app/app.py
+++ callback/app/app.py
@@ -1,16 +1,48 @@
-from flask import Flask, request
-from flask.wrappers import Response
-import xmltodict
 import subprocess
+
+import xmltodict
+from flask import Flask, jsonify, request
+from flask.wrappers import Response
+from pyexpat import ExpatError
+from werkzeug.exceptions import BadRequest, HTTPException
 
 app = Flask(__name__)
 
 @app.route("/callback/audio-youtube", methods=["POST"]) 
-def audio_youtube():
+def audio_youtube() -> Response:
     try:
         data = xmltodict.parse(request.data)
+    except ExpatError:
+        raise BadRequest("XML data could not be parsed")
+
+    try:
         video_id = data["feed"]["entry"]["yt:videoId"]
-    except:
-        return "error", 400
-    subprocess.Popen(["yt-dlp", "--embed-thumbnail", "--embed-metadata", "-x", "--audio-format", "flac", "--audio-quality", "0", "--output", "/yt-media/%(title)s.%(ext)s", "https://www.youtube.com/watch?v=" + video_id])
+        if not len(video_id) == 11:
+            raise BadRequest("Invalid video ID")
+    except KeyError:
+        raise BadRequest("XML data is missing required fields")
+
+    subprocess.Popen(["yt-dlp", "--write-thumbnail", "--write-info-json",
+    "-x", "--audio-format", "flac", "--audio-quality","0",
+    "--output", "/yt-media/%(title)s [%(id)s].%(ext)s", "https://www.youtube.com/watch?v=" + video_id])
+
     return Response(status=204)
+
+@app.errorhandler(HTTPException)
+def handle_http_exceptions(error):
+    return jsonify({
+        "code": error.code,
+        "name": error.name,
+        "error": error.description
+    }), error.code
+
+@app.errorhandler(Exception)
+def handle_internal_exceptions(error: Exception):
+    if isinstance(error, HTTPException):
+        return error
+
+    return jsonify({
+        "code": 500,
+        "name": "Internal Server Error",
+        "error": "An internal server error has occurred"
+    }), 500
